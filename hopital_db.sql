a3f70a0ca09f8a02d943d971023ff42322206ef1
yasmine <yasminelaouini2@gmail.com>
2026-04-23 12:13:01 +0100

    bd modifié par yass

--- hopital_db.sql
+++ hopital_db.sql
@@ -1,20 +1,3 @@
--- --------------------------------------------------------
--- Host:                         127.0.0.1
--- Server version:               8.0.30 - MySQL Community Server - GPL
--- Server OS:                    Win64
--- HeidiSQL Version:             12.1.0.6537
--- --------------------------------------------------------
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8 */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
-
 -- Dumping database structure for hopital_db
 CREATE DATABASE IF NOT EXISTS `hopital_db` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
 USE `hopital_db`;
@@ -28,7 +11,7 @@ CREATE TABLE IF NOT EXISTS `medecins` (
   `telephone` varchar(20) DEFAULT NULL,
   `email` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`id_medecin`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=UTF8MB4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- Dumping data for table hopital_db.medecins: ~0 rows (approximately)
 
@@ -45,9 +28,34 @@ CREATE TABLE IF NOT EXISTS `utilisateurs` (
 -- Dumping data for table hopital_db.utilisateurs: ~1 rows (approximately)
 INSERT INTO `utilisateurs` (`id`, `username`, `password`, `role`) VALUES
 	(1, 'admin', 'admin123', 'ADMIN');
+	
 
-/*!40103 SET TIME_ZONE=IFNULL(@OLD_TIME_ZONE, 'system') */;
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40111 SET SQL_NOTES=IFNULL(@OLD_SQL_NOTES, 1) */;
+-- Table patients
+CREATE TABLE IF NOT EXISTS patients (
+  id_patient     INT AUTO_INCREMENT PRIMARY KEY,
+  nom            VARCHAR(100) NOT NULL,
+  prenom         VARCHAR(100) NOT NULL,
+  cin            VARCHAR(20)  UNIQUE,
+  date_naissance DATE,
+  telephone      VARCHAR(20),
+  email          VARCHAR(150),
+  adresse        VARCHAR(255)
+);
+
+-- Table rendez_vous
+CREATE TABLE IF NOT EXISTS rendez_vous (
+  id_rdv         INT AUTO_INCREMENT PRIMARY KEY,
+  id_patient     INT NOT NULL,
+  id_medecin     INT NOT NULL,
+  date_rdv       DATE NOT NULL,
+  heure_rdv      TIME NOT NULL,
+  motif          VARCHAR(255),
+  statut         ENUM('PLANIFIE','CONFIRME','ANNULE') DEFAULT 'PLANIFIE',
+  FOREIGN KEY (id_patient) REFERENCES patients(id_patient),
+  FOREIGN KEY (id_medecin) REFERENCES medecins(id_medecin)
+);
+
+-- Créer le compte secrétaire par défaut
+INSERT INTO utilisateurs (username, password, role)
+VALUES ('secretaire', 'sec123', 'SECRETAIRE')
+ON DUPLICATE KEY UPDATE username=username;
